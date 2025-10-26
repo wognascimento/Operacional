665826ca01408f2945db654e81830dd7a48f27c7
wesley <wog.nascimento@gmail.com>
2025-10-26 11:03:30 -0300

    CADASTRO TRANSPORTADORA

--- Operacional/DataBase/Models/TranportadoraModel.cs
+++ Operacional/DataBase/Models/TranportadoraModel.cs
@@ -8,16 +8,25 @@ public class TranportadoraModel
 {
     [Key]
     public long? codtransportadora { get; set; }
+    [Required]
     public required string nometransportadora { get; set; }
+    [Required]
     public required string cep { get; set; }
+    [Required]
     public required string endereco { get; set; }
+    [Required]
     public required string bairro { get; set; }
+    [Required]
     public required string cidade { get; set; }
+    [Required]
     public required string uf { get; set; }
     public string? ie { get; set; }
     public string? ccm { get; set; }
+    [Required]
     public required string cnpj { get; set; }
+    [Required]
     public required int ddd { get; set; }
+    [Required]
     public required int fone_1 { get; set; }
     public int? fone_2 { get; set; }
     public string? contato { get; set; }
